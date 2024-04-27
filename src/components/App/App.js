d5398132f95e2585e2d36a21a6863884920a005d
rasskajui <nana7305@mail.ru>
2024-04-28 02:01:35 +0300

    add header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
+import Header from '../Header/Header';
 
 function App() {
 
   return (
     <div className="page">
+      <Header />
       <ThemeSwitcher />
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+function Header(props) {
+    return (
+        <header className="header">
+            <Link to='/' className='header__logo'>
+                <p className="header__logo-title logo">Color
+                <span style={{color: '#f00'}}> p</span>
+                <span style={{color: '#FF7A00'}}>a</span>
+                <span style={{color: '#FFC700'}}>l</span>
+                <span style={{color: '#00D522'}}>e</span>
+                <span style={{color: '#00C2FF'}}>t</span>
+                <span style={{color: '#000AFF'}}>t</span>
+                <span style={{color: '#6B0CA6'}}>e </span>
+                generator</p>
+            </Link>
+            <input type="checkbox" id="check" className="header__menu-check"/>
+            <label htmlFor="check" className="header__menu-btn"></label>
+            <nav className='header__nav'>
+                <ul className='header__nav-list'>
+                    <li className='header__nav-item'>
+                        <Link to='/register' className='header__nav-btn'>Зарегистрироваться</Link>
+                    </li>
+                    <li className='header__nav-item'>
+                        <Link to='/login' className='header__nav-btn header__nav-btn_accent'>Войти</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
