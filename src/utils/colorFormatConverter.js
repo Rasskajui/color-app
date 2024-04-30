bd4375acf931adc79e774a63ed8ba92e5ab02b6f
rasskajui <nana7305@mail.ru>
2024-04-30 20:17:22 +0300

    color gen and format

---
+++ src/utils/colorFormatConverter.js
@@ -0,0 +1,26 @@
+export const hslToHex = (h, s, l) => {
+    l /= 100;
+    const a = s * Math.min(l, 1 - l) / 100;
+    const f = n => {
+        const k = (n + h / 30) % 12;
+        const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+        return Math.round(255 * color).toString(16).padStart(2, '0');
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+}
+
+export const hslToRgb = (h, s, l) => {
+    let c = (1 - Math.abs(2 * l - 1)) * s;
+    let x = c * (1 - Math.abs((h / 60) % 2 - 1));
+    let m = l - c / 2;
+
+    let r = h < 60 || h >= 300 ? c : h < 120 || h >= 240 ? x : 0;
+    let g = h >= 240 ? 0 : h >= 180 || h < 60 ? x : c;
+    let b = h < 120 ? 0 : h < 180 || h >= 300 ? x : c;
+    
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}   
---
+++ src/utils/paletteGenerator.js
@@ -0,0 +1,51 @@
+export const generateMonochromaticPalette = (hsl, count) => {
+    const palette = [];
+    const [hue, saturation, lightness] = hsl;
+    const dropPercent = Math.floor((100 / count));
+    for (let i = 0; i < count; i++)
+        palette.push([hue, saturation, (lightness + dropPercent * i) % 100])
+    palette.sort((a, b) => b[2] - a[2])
+    return palette;
+}
+
+export const generateAnalogousPalette = (hsl, count) => {
+    const palette = [];
+    const [hue, saturation, lightness] = hsl;
+    for (let i = 0; i < count; i++)
+        palette.push([(hue + 15 * i) % 360, saturation, lightness])
+    palette.sort((a, b) => b[0] - a[0])
+    return palette;
+}
+
+export const generateCompoundPalette = (hsl, count) => {
+    const palette = [];
+    let [hue, saturation, lightness] = hsl;
+    palette.push(hsl);
+    for (let i = 1; i < count; i++) {
+        if (i % 2 !== 0) 
+            hue = (hue + 180) % 360
+        else
+            hue = (hue + 30) % 360
+        palette.push([hue, saturation, lightness])
+    }
+    palette.sort((a, b) => b[0] - a[0])
+    return palette;
+}
+
+export const generateTriadicPalette = (hsl, count) => {
+    const palette = [];
+    let [hue, saturation, lightness] = hsl;
+    palette.push(hsl);
+    for (let i = 1; i < count; i++) {
+        if (i % 3 !== 0)
+            hue = (hue + 120) % 360
+        else 
+            hue = (hue + 30) % 360
+        palette.push([hue, saturation, lightness])
+    }
+    return palette;
+}
+
+export const generateRandomHexColor = () => {
+    return '#' + Math.floor(Math.random()*16777215).toString(16)
+} 
